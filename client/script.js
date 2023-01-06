d3fa093b217fe93e047de86a6dbd61844d302052
unknown <yliu261@u.rochester.edu>
2023-01-06 05:52:40 -0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-2xsm.onrender.com', {
+    const response = await fetch('https://codex-2xsm.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -95,6 +95,7 @@ const handleSubmit = async (e) => {
             prompt: data.get('prompt')
         })
     })
+    console.loh(response);
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
